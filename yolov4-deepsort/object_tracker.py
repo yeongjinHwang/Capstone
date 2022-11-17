b3f7820e657d65385a44856fffdbb17cf8d8e6d9
yeongjin.gongjin <yeongjin.gongjin@gmail.com>
2022-11-18 02:17:41 +0900

    cashPos

--- yolov4-deepsort/object_tracker.py
+++ yolov4-deepsort/object_tracker.py
@@ -68,12 +68,16 @@ def main(_argv):
 
     ####계산대 좌표 받아오기####
     firstReturn, firstFrame = vid.read()
-    x_pos, y_pos, calWidth, calHeight = cv2.selectROI("location",firstFrame,False)
+    firstReturn2, firstFrame2 = vid2.read()
+    firstFrame = cv2.flip(firstFrame,1)
+    firstFrame2 = cv2.flip(firstFrame2,1)
+    resultPos = cv2.hconcat([firstFrame, firstFrame2])
+    xPos, yPos, calWidth, calHeight = cv2.selectROI("location",resultPos,False)
     cv2.destroyAllWindows()
-    x2_pos, y2_pos, calWidth, calHeight = cv2.selectROI("location",firstFrame,False)
+    x2Pos, y2Pos, calWidth2, calHeight2 = cv2.selectROI("location",resultPos,False)
     cv2.destroyAllWindows()
-    print("cal1 location : x "+str(x_pos)+" y "+str(y_pos))
-    print("cal2 location : x "+str(x2_pos)+" y "+str(y2_pos))
+    print("cal1 location : x "+str(xPos)+" y "+str(yPos)+" width "+str(calWidth)+" height "+str(calHeight))
+    print("cal2 location : x "+str(x2Pos)+" y "+str(y2Pos)+" width "+str(calWidth2)+" height "+str(calHeight2))
 
     ####--output path 인자로 시작하면 저장하기 위한 코드####
     if FLAGS.output:
@@ -84,119 +88,125 @@ def main(_argv):
         out = cv2.VideoWriter(FLAGS.output, codec, fps, (width, height))
 
     ####영상or웹캠실행####
+    frameDrop=0
     while True:
-        ####프레임 받아오고 FLIP으로 좌우반전####
-        return_value, frame = vid.read()
-        return_value2, frame2 = vid2.read()
-        frame = cv2.flip(frame,1)
-        frame2 = cv2.flip(frame2,1)
-        ####opencv는 color를 bgr 방식으로 저장하는데, 이를 rgb방식으로 변환####
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        ####넘파이로 저장된 이미지 배열을 PIL 이미지로 변환####
-        image = Image.fromarray(frame)
+        frameDrop=frameDrop+1
+        if frameDrop%2==0 :
+            ####프레임 받아오고 FLIP으로 좌우반전####
+            return_value, frame = vid.read()
+            return_value2, frame2 = vid2.read()
+            frame = cv2.flip(frame,1)
+            frame2 = cv2.flip(frame2,1)
+            ####opencv는 color를 bgr 방식으로 저장하는데, 이를 rgb방식으로 변환####
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            ####넘파이로 저장된 이미지 배열을 PIL 이미지로 변환####
+            image = Image.fromarray(frame)
 
-        ####SIZE변경####
-        frame_size = (frame.shape[:2])
-        image_data = cv2.resize(frame, (input_size, input_size))
-        image_data = image_data / 255.
-        image_data = image_data[np.newaxis, ...].astype(np.float32)
+            ####SIZE변경####
+            frame_size = (frame.shape[:2])
+            image_data = cv2.resize(frame, (input_size, input_size))
+            image_data = image_data / 255.
+            image_data = image_data[np.newaxis, ...].astype(np.float32)
 
-        start_time = time.time() ##FPS 구해서 출력하려고
-        batch_data = tf.constant(image_data)
-        pred_bbox = infer(batch_data)
-        for key, value in pred_bbox.items():
-            boxes = value[:, :, 0:4]
-            pred_conf = value[:, :, 4:]
+            start_time = time.time() ##FPS 구해서 출력하려고
+            batch_data = tf.constant(image_data)
+            pred_bbox = infer(batch_data)
+            for key, value in pred_bbox.items():
+                boxes = value[:, :, 0:4]
+                pred_conf = value[:, :, 4:]
 
-        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
-            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
-            scores=tf.reshape(
-                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
-            max_output_size_per_class=50,
-            max_total_size=50,
-            iou_threshold=FLAGS.iou,
-            score_threshold=FLAGS.score
-        )
+            boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+                boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+                scores=tf.reshape(
+                    pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+                max_output_size_per_class=50,
+                max_total_size=50,
+                iou_threshold=FLAGS.iou,
+                score_threshold=FLAGS.score
+            )
 
-        num_objects = valid_detections.numpy()[0]
-        bboxes = boxes.numpy()[0]
-        bboxes = bboxes[0:int(num_objects)]
-        scores = scores.numpy()[0]
-        scores = scores[0:int(num_objects)]
-        classes = classes.numpy()[0]
-        classes = classes[0:int(num_objects)]
+            num_objects = valid_detections.numpy()[0]
+            bboxes = boxes.numpy()[0]
+            bboxes = bboxes[0:int(num_objects)]
+            scores = scores.numpy()[0]
+            scores = scores[0:int(num_objects)]
+            classes = classes.numpy()[0]
+            classes = classes[0:int(num_objects)]
 
-        original_h, original_w, _ = frame.shape
-        bboxes = utils.format_boxes(bboxes, original_h, original_w)
+            original_h, original_w, _ = frame.shape
+            bboxes = utils.format_boxes(bboxes, original_h, original_w)
 
-        pred_bbox = [bboxes, scores, classes, num_objects]
-        class_names = utils.read_class_names(cfg.YOLO.CLASSES)
-        allowed_classes = ['person']
+            pred_bbox = [bboxes, scores, classes, num_objects]
+            class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+            allowed_classes = ['person']
 
-        names = []
-        deleted_indx = []
-        for i in range(num_objects):
-            class_indx = int(classes[i])
-            class_name = class_names[class_indx]
-            if class_name not in allowed_classes:
-                deleted_indx.append(i)
-            else:
-                names.append(class_name)
-        names = np.array(names)
-        count = len(names)
+            names = []
+            deleted_indx = []
+            for i in range(num_objects):
+                class_indx = int(classes[i])
+                class_name = class_names[class_indx]
+                if class_name not in allowed_classes:
+                    deleted_indx.append(i)
+                else:
+                    names.append(class_name)
+            names = np.array(names)
+            count = len(names)
 
-        # delete detections that are not in allowed_classes
-        bboxes = np.delete(bboxes, deleted_indx, axis=0)
-        scores = np.delete(scores, deleted_indx, axis=0)
+            # delete detections that are not in allowed_classes
+            bboxes = np.delete(bboxes, deleted_indx, axis=0)
+            scores = np.delete(scores, deleted_indx, axis=0)
 
-        # encode yolo detections and feed to tracker
-        features = encoder(frame, bboxes)
-        detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(bboxes, scores, names, features)]
+            # encode yolo detections and feed to tracker
+            features = encoder(frame, bboxes)
+            detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(bboxes, scores, names, features)]
 
-        #initialize color map
-        cmap = plt.get_cmap('tab20b')
-        colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
+            #initialize color map
+            cmap = plt.get_cmap('tab20b')
+            colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
 
-        # run non-maxima supression
-        boxs = np.array([d.tlwh for d in detections])
-        scores = np.array([d.confidence for d in detections])
-        classes = np.array([d.class_name for d in detections])
-        indices = preprocessing.non_max_suppression(boxs, classes, nms_max_overlap, scores)
-        detections = [detections[i] for i in indices]       
+            # run non-maxima supression
+            boxs = np.array([d.tlwh for d in detections])
+            scores = np.array([d.confidence for d in detections])
+            classes = np.array([d.class_name for d in detections])
+            indices = preprocessing.non_max_suppression(boxs, classes, nms_max_overlap, scores)
+            detections = [detections[i] for i in indices]       
 
-        # Call the tracker
-        tracker.predict()
-        tracker.update(detections)
-        # update tracks
-        for track in tracker.tracks:
-            if not track.is_confirmed() or track.time_since_update > 1:
-                continue 
-            bbox = track.to_tlbr()
-            class_name = track.get_class()
-            ####바운딩박스, text 등등 삽입####
-            color = colors[int(track.track_id) % len(colors)]
-            color = [i * 255 for i in color]
-            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
-            x=int((bbox[0]+bbox[2])/2)
-            y=int((bbox[1]+bbox[3])/2)
-            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
-            cv2.putText(frame,"x: "+str(x)+" y:"+str(y),(int(bbox[0]), int(bbox[1]-5)),0,0.5,(255,255,255),2)
-            cv2.putText(frame, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-20)),0, 0.75, (255,255,255),2)
+            # Call the tracker
+            tracker.predict()
+            tracker.update(detections)
+            # update tracks
+            for track in tracker.tracks:
+                if not track.is_confirmed() or track.time_since_update > 1:
+                    continue 
+                bbox = track.to_tlbr()
+                class_name = track.get_class()
+                ####바운딩박스, text 등등 삽입####
+                color = colors[int(track.track_id) % len(colors)]
+                color = [i * 255 for i in color]
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+                x=int((bbox[0]+bbox[2])/2)
+                y=int((bbox[1]+bbox[3])/2)
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
+                cv2.putText(frame,"x: "+str(x)+" y:"+str(y),(int(bbox[0]), int(bbox[1]-5)),0,0.5,(255,255,255),2)
+                cv2.putText(frame, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-20)),0, 0.75, (255,255,255),2)
+            ####FPS####
+            fps = 1.0 / (time.time() - start_time) *2
+            result = np.asarray(frame)
+            result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-        ####FPS####
-        fps = 1.0 / (time.time() - start_time)
-        cv2.putText(frame, str(int(fps)), (int(width-100), 100), 0,0.75, (255,255,255),2)
-        result = np.asarray(frame)
-        result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            ####이미지합치기####
+            result = cv2.hconcat([result, frame2])
+            cv2.putText(result, str(int(fps)), (int(width), 50), 0,1, (255,255,255),2)
 
-        ####이미지합치기####
-        result = cv2.hconcat([result, frame2])
-        cv2.imshow("Output Video", result)
-        
-        ####output파일저장####
-        if FLAGS.output:
-            out.write(result)
-        if cv2.waitKey(1) & 0xFF == ord('q') : break
+            ####계산대그리기####
+            cv2.rectangle(result, (int(xPos), int(yPos)), (int(xPos+calWidth), int(yPos+calHeight)), (0,255,0), 2)
+            cv2.rectangle(result, (int(x2Pos), int(y2Pos)), (int(x2Pos+calWidth2), int(y2Pos+calHeight2)), (0,255,0), 2)
+            cv2.imshow("Output Video", result)
+            
+            ####output파일저장####
+            if FLAGS.output:
+                out.write(result)
+            if cv2.waitKey(1) & 0xFF == ord('q') : break
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
